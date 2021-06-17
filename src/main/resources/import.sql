f42eecf88faaca7fce26a8d072dfe57600c7e10e
Carlos Laurine <carlos.pinho@ucsal.edu.br>
2021-06-17 02:58:08 -0300

    Offer Entity Created + Mapping + Mandatory Relation (Many-To-one with Course Entity) + Database Seeding tested at H2 DB

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -16,3 +16,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Java Bootcamp', 'https://www.tutorialspoint.com/videotutorials/assets/videos/courses/896/images/course_896_image.png', 'https://i.pcmag.com/imagery/articles/00aKX2mKSN9x94d96DSqolA-1..1596625018.jpg');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0.0', TIMESTAMP WITH TIME ZONE '2020-10-15T17:30:00Z', TIMESTAMP WITH TIME ZONE '2021-12-15T12:30:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.5.0', TIMESTAMP WITH TIME ZONE '2020-12-15T17:30:00Z', TIMESTAMP WITH TIME ZONE '2022-02-15T12:30:00Z', 1);
