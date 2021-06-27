c4522037921785c649a4a270695917ca24e57993
Carlos Laurine <carlos.pinho@ucsal.edu.br>
2021-06-27 00:49:40 -0300

    Fix: Additional Relations Completed

--- src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dslearnbds.entities;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Entity;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -26,10 +29,11 @@ public class User {
 	private String password;
 
 	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "tb_user_role",
-	joinColumns = @JoinColumn(name = "user_id"),
-	inverseJoinColumns = @JoinColumn(name = "role_id"))
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
+
+	@OneToMany(mappedBy = "user")
+	private List<Notification> notifications = new ArrayList<>();
 
 	public User() {
 
@@ -77,6 +81,10 @@ public class User {
 
 	public Set<Role> getRoles() {
 		return roles;
+	}
+
+	public List<Notification> getNotifications() {
+		return notifications;
 	}
 
 	@Override
