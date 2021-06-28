ac5ebaaf743e71acc729087740ba42fa4c954f36
Carlos Laurine <carlos.pinho@ucsal.edu.br>
2021-06-28 02:14:05 -0300

    Security and Validation Classes + Exception Handling Implemented and Tested at Postman Customized Collecion

---
+++ src/main/java/com/devsuperior/dslearnbds/services/exceptions/DBException.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dslearnbds.services.exceptions;
+
+public class DBException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DBException(String message) {
+		super(message);
+	}
+}
