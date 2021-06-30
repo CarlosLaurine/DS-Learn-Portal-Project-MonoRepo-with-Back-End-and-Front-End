3f526e210a4cb538ec950122fe6584df5f9182cf
Carlos Laurine <carlos.pinho@ucsal.edu.br>
2021-06-30 03:30:20 -0300

    Custom Pagination Content For Current Users Implemented at Notification Service's FindAll Method

---
+++ src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dslearnbds.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dslearnbds.dto.NotificationDTO;
+import com.devsuperior.dslearnbds.entities.Notification;
+import com.devsuperior.dslearnbds.entities.User;
+import com.devsuperior.dslearnbds.repositories.NotificationRepository;
+
+@Service
+public class NotificationService {
+
+	@Autowired
+	private NotificationRepository repository;
+
+	@Autowired
+	private AuthorizationService authorizationService;
+
+	@Transactional(readOnly = true)
+	public Page<NotificationDTO> findNotificationsForActiveUserPaged(Pageable pageable) {
+
+		User currentUser = authorizationService.getCurrentAuthenticatedUser();
+
+		Page<Notification> page = repository.findByUser(currentUser, pageable);
+
+		return page.map(notification -> new NotificationDTO(notification));
+	}
+}
